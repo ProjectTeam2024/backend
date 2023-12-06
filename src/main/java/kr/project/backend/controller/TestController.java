9496b436c9bd6a3b880ac510a4c03d5a3e596610
yjkim-mtn <newy12@naver.com>
2023-12-06 16:29:08 +0900

    #feat. jenkins test5

--- src/main/java/kr/project/backend/controller/TestController.java
+++ src/main/java/kr/project/backend/controller/TestController.java
@@ -1,4 +1,13 @@
 package kr.project.backend.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TestController {
+
+    @GetMapping("/")
+    public String get(){
+        return "1번 서버임!";
+    }
 }
