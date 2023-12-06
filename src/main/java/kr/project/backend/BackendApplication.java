47c0b82e9aad53748abb27c5a05e3bfec61f051f
yjkim-mtn <newy12@naver.com>
2023-12-06 09:23:08 +0900

    #feat. 경로 수정

--- src/main/java/kr/project/backend/BackendApplication.java
+++ src/main/java/kr/project/backend/BackendApplication.java
@@ -1,4 +1,4 @@
-package com.example.backend;
+package kr.project.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/kr/project/backend/BackendApplicationTests.java
+++ src/test/java/kr/project/backend/BackendApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.backend;
+package kr.project.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
