f23dfc93a518fde3e603fd283507de66863d2a1f
youngjae <newy12@naver.com>
2023-12-09 00:15:32 +0900

    #feat 레디스추가

--- src/main/java/kr/project/backend/controller/TestController.java
+++ src/main/java/kr/project/backend/controller/TestController.java
@@ -1,12 +1,13 @@
 package kr.project.backend.controller;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+    @Cacheable(value = "test")
     @GetMapping("/")
     public String get(){
         return "마지막테스트22";
