2b6d84c16fbd2cd7fa0d36adeccdec4db172cb87
yjkim-mtn <newy12@naver.com>
2023-12-19 13:40:55 +0900

    #feat. 스웨거 시큐리티 추가

--- src/main/java/kr/project/backend/user/coin/controller/StakingInfoController.java
+++ src/main/java/kr/project/backend/user/coin/controller/StakingInfoController.java
@@ -1,6 +1,7 @@
 package kr.project.backend.user.coin.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import kr.project.backend.common.Environment;
 import kr.project.backend.user.coin.model.StakingInfoResponseDto;
 import kr.project.backend.user.coin.service.StakingInfoService;
@@ -19,7 +20,7 @@ public class StakingInfoController {
 
     private final StakingInfoService stakingInfoService;
 
-    @Operation(summary = "코인 스테이킹 목록 조회", description = "코인 스테이킹 목록 조회를 합니다.")
+    @Operation(summary = "코인 스테이킹 목록 조회", description = "코인 스테이킹 목록 조회를 합니다.",security = @SecurityRequirement(name = "Authorization"))
     @GetMapping("/infos")
     public ResponseEntity<List<StakingInfoResponseDto>> stakingInfos(){
         return ResponseEntity.ok(stakingInfoService.getStakingInfos());
