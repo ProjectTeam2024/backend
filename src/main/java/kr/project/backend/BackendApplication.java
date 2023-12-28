4b29380e61a86baf7ceddcc5319bf9bbab2460e2
yjkim-mtn <newy12@naver.com>
2023-12-28 13:10:47 +0900

    #feat. 외부 application.yml 실행 설정

--- src/main/java/kr/project/backend/BackendApplication.java
+++ src/main/java/kr/project/backend/BackendApplication.java
@@ -3,13 +3,18 @@ package kr.project.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class BackendApplication {
 
+    public static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "optional:classpath:application-local.yml"
+            + ", optional:/app/project/config/application.yml";
     public static void main(String[] args) {
-        SpringApplication.run(BackendApplication.class, args);
-
+        new SpringApplicationBuilder(BackendApplication.class)
+                .properties(APPLICATION_LOCATIONS)
+                .run(args);
     }
 
 }
