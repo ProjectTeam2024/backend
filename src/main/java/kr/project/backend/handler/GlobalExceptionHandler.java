4c7b180d22bcbc2a06a567a9074acf50b7dcf165
namgihyeon <skarlgus0419@naver.com>
2023-12-29 01:09:03 +0900

    #feat. BadRequest 예외 핸들러 추가

--- src/main/java/kr/project/backend/handler/GlobalExceptionHandler.java
+++ src/main/java/kr/project/backend/handler/GlobalExceptionHandler.java
@@ -2,18 +2,18 @@ package kr.project.backend.handler;
 
 import jakarta.servlet.http.HttpServletRequest;
 import kr.project.backend.common.Response;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @Slf4j
-@ControllerAdvice(basePackages = {"kr.project.backend"})
-@RequiredArgsConstructor
+@ControllerAdvice
+
 @Order(Ordered.LOWEST_PRECEDENCE)
 public class GlobalExceptionHandler {
 
@@ -25,6 +25,16 @@ public class GlobalExceptionHandler {
         r.setMsg("server error 발생");
 
         return new ResponseEntity<>(r, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Response<Void>> bindExceptionHandler(HttpServletRequest httpServletRequest, MethodArgumentNotValidException e){
+
+        Response<Void> r = new Response<>();
+        r.setCode("9998");
+        r.setMsg("parameter 누락 ( "+e.getBindingResult().getFieldErrors().getFirst().getField()+" )");
+
+        return new ResponseEntity<>(r, HttpStatus.BAD_REQUEST);
     }
 
 }
