01c1ed2320e5b4cb5667ebbc0e2e9c4818b18c6d
namgihyeon <skarlgus0419@naver.com>
2024-01-02 16:19:27 +0900

    #fix. access토큰 재발급 api dto 수정

--- src/main/java/kr/project/backend/dto/user/UserRefreshTokenRequestDto.java
+++ src/main/java/kr/project/backend/dto/user/UserRefreshTokenRequestDto.java
@@ -1,9 +1,13 @@
 package kr.project.backend.dto.user;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.util.UUID;
 
 @Data
 public class UserRefreshTokenRequestDto {
+
+    @NotNull
     private UUID refreshTokenId;
 }
