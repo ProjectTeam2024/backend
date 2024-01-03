3f21df1e25a8853e04ce43ea2f438369da7d5a23
yjkim-mtn <newy12@naver.com>
2024-01-03 15:49:28 +0900

    #feat. 서버 시간 세팅

--- src/main/java/kr/project/backend/BackendApplication.java
+++ src/main/java/kr/project/backend/BackendApplication.java
@@ -1,9 +1,12 @@
 package kr.project.backend;
 
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.TimeZone;
 
 @EnableJpaAuditing
 @SpringBootApplication
@@ -12,6 +15,11 @@ public class BackendApplication {
     public static final String APPLICATION_LOCATIONS = "spring.config.location="
             + "optional:classpath:application-local.yml"
             + ", optional:/app/project/config/application.yml";
+    @PostConstruct
+    public void started() {
+        // timezone UTC 셋팅
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
     public static void main(String[] args) {
         new SpringApplicationBuilder(BackendApplication.class)
                 .properties(APPLICATION_LOCATIONS)
